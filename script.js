759aed0344adc2283651b0be4c54758bd1e1ea33
Genivaldo2230 <gda_anjos@hotmail.com.br>
2023-07-02 04:29:00 -0300

    atualização de fotos

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle('light') /*codigo refactored usando o toggle*/
+  html.classList.toggle("light") /*codigo refactored usando o toggle*/
 
   //pega img na taqg #profile
-  const img = document.querySelector('#profile img')
+  const img = document.querySelector("#profile img")
 
-  if (html.classList.contains('light')) { /*linha e condicional boolean*/
-    //se tiver light mode. adiciona a imagen light   
+  if (html.classList.contains("light")) {
+    /*linha e condicional boolean*/
+    //se tiver light mode. adiciona a imagen light
     img.setAttribute("src", "./assents/Gda-Light.png")
   } else {
-    img.setAttribute('src', './assents/avatar.png')
+    img.setAttribute("src", "./assents/avatar-gda.png")
   }
-
 }

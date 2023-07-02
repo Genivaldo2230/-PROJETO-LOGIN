53c0a0717a3eb4cf8f987629ccdcf625bd1d1b7b
Genivaldo2230 <gda_anjos@hotmail.com.br>
2023-07-02 02:44:23 -0300

    :tada: initial commit rockeseat-cursos

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle('light') /*codigo refactored usando o toggle*/
+
+  //pega img na taqg #profile
+  const img = document.querySelector('#profile img')
+
+  if (html.classList.contains('light')) { /*linha e condicional boolean*/
+    //se tiver light mode. adiciona a imagen light   
+    img.setAttribute("src", "./assents/Gda-Light.png")
+  } else {
+    img.setAttribute('src', './assents/avatar.png')
+  }
+
+}
